e02f58470df269f5b3116225017200437b67a2f0
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-03 22:49:22 +1000

    Create calc.js

---
+++ src/utils/calc.js
@@ -0,0 +1,25 @@
+export class Calculator {
+    static hasArb(odds1, odds2) {
+        return ((1 / odds1) + (1 / odds2) < 1);
+    }
+
+    static moneySplit(total, odds1, odds2) {
+        let money1;
+        let money2;
+        if (odds2 >= odds1) {
+            money2 = total / odds2;
+            money1 = total - money2;
+        } else {
+            money1 = total / odds1;
+            money2 = total - money1;
+        }
+        return {
+            money1: money1,
+            money2: money2,
+        };
+    }
+
+    static profit(odds1, odds2) {
+        return (1 - (1 / odds1) - (1 / odds2));
+    }
+}
