e302753718e6cd7183e5aaa30ff98d8b89b15fb0
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-04 13:41:24 +1000

    Had to call the main function
    Now works with getting stuff but need to get better at using CSS selecting

--- src/scrapers/index.js
+++ src/scrapers/index.js
@@ -3,17 +3,14 @@ import * as cheerio from 'cheerio';
 
 async function main() {
     try{
+        console.log('HELLO!!!!!!!!!!');
         const pageHTML = await axios.get("https://www.pitpanda.rocks/players/kanye_fan30");
         const $ = cheerio.load(pageHTML.data);
-        console.log($.html());
-        const card = $('#side .Card').filter(function() {
-            return $(this).find('.Card-Head').text().trim() === 'Leaderboard Positions';
-        });
-        
-        const cardBodyContent = card.find('.Card-Body').text().trim();
-        
-        console.log(cardBodyContent);
+        const elements = $('#side .Card .Card-Body');
+        console.log(elements);
     } catch (error) {
         console.log('Error fetching page: ', error);
     }
 }
+
+main();
