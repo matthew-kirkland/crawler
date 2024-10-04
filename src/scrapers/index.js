84c4683a559917e0681cd9145c3b3487251b6827
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-05 00:09:10 +1000

    Works except pitpanda is not statically loaded so a different parsing technique is required

--- src/scrapers/index.js
+++ src/scrapers/index.js
@@ -3,11 +3,33 @@ import * as cheerio from 'cheerio';
 
 async function main() {
     try{
-        console.log('HELLO!!!!!!!!!!');
         const pageHTML = await axios.get("https://www.pitpanda.rocks/players/kanye_fan30");
         const $ = cheerio.load(pageHTML.data);
-        const elements = $('#side .Card .Card-Body');
-        console.log(elements);
+        console.log($.html());
+        const card = $('#root #side .Card');
+
+        // const card = $('#root #side .Card').filter(function() {
+        //     return $(this).find('.Card-Head').text().trim() == 'Leaderboard Positions';
+        // });
+        // console.log(card);
+        const leaderboardPositions = card.find('.Card-Body div span').map(function() {
+            return $(this).find('span').text().trim();
+        }).get();
+        console.log(leaderboardPositions);
+
+        const positions = [];
+
+        leaderboardPositions.forEach(function(position) {
+            positions.push(parseInt(position.replace(/[^\d]/g, '')));
+        });
+        let sum = 0;
+        let i = 0;
+        for (position of positions) {
+            sum += position;
+            i++;
+        }
+        console.log(sum / i);
+        
     } catch (error) {
         console.log('Error fetching page: ', error);
     }
