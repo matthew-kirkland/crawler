ce4aae79e71f07f838c057f8fcedcdcdedafea02
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-09 22:55:26 +1100

    Added actual puppeteer npm and got it working on pitpanda
    Scraped leaderboard positions from the side card

--- src/scrapers/index.js
+++ src/scrapers/index.js
@@ -1,35 +1,38 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
 
 async function main() {
-    try{
-        const pageHTML = await axios.get("https://www.pitpanda.rocks/players/kanye_fan30");
-        const $ = cheerio.load(pageHTML.data);
-        console.log($.html());
-        const card = $('#root #side .Card');
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.pitpanda.rocks/players/kanye_fan30');
 
-        // const card = $('#root #side .Card').filter(function() {
-        //     return $(this).find('.Card-Head').text().trim() == 'Leaderboard Positions';
-        // });
-        // console.log(card);
-        const leaderboardPositions = card.find('.Card-Body div span').map(function() {
-            return $(this).find('span').text().trim();
-        }).get();
-        console.log(leaderboardPositions);
+        await page.waitForSelector('#root #side .Card');
 
-        const positions = [];
-
-        leaderboardPositions.forEach(function(position) {
-            positions.push(parseInt(position.replace(/[^\d]/g, '')));
+        const leaderboardPositions = await page.evaluate(() => {
+            const cards = document.querySelectorAll('#root #side .Card');
+            const positions = [];
+            cards.forEach(card => {
+                const text = card.querySelector('.Card-Head').innerText.trim();
+                if (text == 'Leaderboard Positions') {
+                    const spans = card.querySelectorAll('.Card-Body div span');
+                    const cardPositions = Array.from(spans).map(span => {
+                        const rawText = span.innerText.trim();
+                        return parseInt(rawText.replace(/[^0-9]/g, ''));
+                    });
+                    positions.push(...cardPositions);
+                }
+            });
+            return positions;
         });
+
+        console.log(`Leaderboard positions are: ${leaderboardPositions}`);
         let sum = 0;
-        let i = 0;
-        for (position of positions) {
-            sum += position;
-            i++;
+        for (let i = 0; i < leaderboardPositions.length; i++) {
+            sum += leaderboardPositions[i];
         }
-        console.log(sum / i);
-        
+        console.log(`The average of the leaderboard positions is: ${sum / leaderboardPositions.length}`);
+
+        await browser.close();
     } catch (error) {
         console.log('Error fetching page: ', error);
     }
