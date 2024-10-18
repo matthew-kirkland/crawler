22af41f3c7818a6972cbac9292e017d2ae96f42b
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-19 01:28:28 +1100

    FIXED MOBILE VS DESKTOP ISSUE

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import puppeteer from 'puppeteer';
 async function main() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    // VIEWPORT IS THE THING INFLUENCING MOBILE OR DESKTOP VERSION !!!!!!!!!!!!!!!!
+    // await page.setViewport({
+    //     width: 1280,
+    //     height: 800
+    // });
+
+
     // const leaderboardPositions = await pitpandaScraper(page, 'https://www.pitpanda.rocks/players/kanye_fan30');
     // console.log(`Leaderboard positions are: ${leaderboardPositions}`);
     // let sum = 0;
