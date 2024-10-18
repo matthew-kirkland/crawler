b887e211fac1a8bea3f90b60d162e76cceb508d9
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-19 01:23:36 +1100

    Added more waitForSelectors to ensure all is loaded

--- src/scrapers/ladbrokes.js
+++ src/scrapers/ladbrokes.js
@@ -5,15 +5,14 @@ export async function ladbrokesScraper(page, url) {
         await page.goto(url);
         console.log('Page loaded');
         // most likely this is the selector for any sport event on ladbrokes
-        // await page.waitForSelector('#app #main #page .sport-event-card', {timeout: 300000});
-        await page.waitForSelector('#app #main #page .sports-event-entry-with-markets', {timeout: 300000});
-        console.log('Selector found');
+        await page.waitForSelector('#app #main #page .sports-event-entry-with-markets');
+        await page.waitForSelector('.sports-event-subtitle__name-text');
+        await page.waitForSelector('.price-button-odds-price span');
+        await page.waitForSelector('.price-button-name span');
         const events = await page.evaluate(() => {
             const eventsArray = [];
             const eventCards = document.querySelectorAll('#main #page .sports-event-entry-with-markets');
-            console.log(`eventcards: ${eventCards}`);
             eventCards.forEach(eventCard => {
-                console.log('Event card found');
                 const title = eventCard.querySelector('.sports-event-subtitle__name-text').innerText.trim();
                 const odds = eventCard.querySelectorAll('.price-button-odds-price span');
                 const names = eventCard.querySelectorAll('.price-button-name span');
@@ -25,7 +24,6 @@ export async function ladbrokesScraper(page, url) {
                     team2Name: names[2].innerText.trim(),
                     team2Odds: odds[2].innerText.trim(),
                 });
-                console.log('Event added');
             });
             return eventsArray;
         });
