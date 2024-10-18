d84cafa9a2dad51e0b37d7493daf66f627bbabb8
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-19 01:01:15 +1100

    Used the mobile selectors to load the content
    Works but only on the non-first attempt, in quick succession.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { pitpandaScraper } from './scrapers/pitpanda.js';
 import puppeteer from 'puppeteer';
 
 async function main() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     // const leaderboardPositions = await pitpandaScraper(page, 'https://www.pitpanda.rocks/players/kanye_fan30');
     // console.log(`Leaderboard positions are: ${leaderboardPositions}`);
@@ -13,7 +13,13 @@ async function main() {
     // }
     // console.log(`The average of the leaderboard positions is: ${sum / leaderboardPositions.length}`);
     const events = await ladbrokesScraper(page, 'https://www.ladbrokes.com.au/sports/soccer/uk-ireland/premier-league');
-    console.log(events);
+    for (const event of events) {
+        console.log(`Event: ${event.eventTitle}`);
+        console.log(`Team 1: ${event.team1Name} - ${event.team1Odds}`);
+        console.log(`Draw: ${event.drawOdds}`);
+        console.log(`Team 2: ${event.team2Name} - ${event.team2Odds}`);
+    }
+    // console.log(`Events array: ${events}`);
     await browser.close();
 }
 
--- src/scrapers/ladbrokes.js
+++ src/scrapers/ladbrokes.js
@@ -5,23 +5,25 @@ export async function ladbrokesScraper(page, url) {
         await page.goto(url);
         console.log('Page loaded');
         // most likely this is the selector for any sport event on ladbrokes
-        await page.waitForSelector('#app #main #page .page-content-inner .sport-event-card');
+        // await page.waitForSelector('#app #main #page .sport-event-card', {timeout: 300000});
+        await page.waitForSelector('#app #main #page .sports-event-entry-with-markets', {timeout: 300000});
         console.log('Selector found');
         const events = await page.evaluate(() => {
             const eventsArray = [];
-            const eventCards = document.querySelectorAll('#main .sport-event-card');
+            const eventCards = document.querySelectorAll('#main #page .sports-event-entry-with-markets');
+            console.log(`eventcards: ${eventCards}`);
             eventCards.forEach(eventCard => {
                 console.log('Event card found');
-                const title = eventCard.querySelector('.sports-event-title__name-text').innerText.trim();
+                const title = eventCard.querySelector('.sports-event-subtitle__name-text').innerText.trim();
                 const odds = eventCard.querySelectorAll('.price-button-odds-price span');
                 const names = eventCard.querySelectorAll('.price-button-name span');
                 eventsArray.push({
                     eventTitle: title,
-                    team1Name: names[0].innerText,
-                    team1Odds: odds[0].innerText,
-                    drawOdds: odds[1].innerText,
-                    team2Name: names[1].innerText,
-                    team2Odds: odds[2].innerText,
+                    team1Name: names[0].innerText.trim(),
+                    team1Odds: odds[0].innerText.trim(),
+                    drawOdds: odds[1].innerText.trim(),
+                    team2Name: names[2].innerText.trim(),
+                    team2Odds: odds[2].innerText.trim(),
                 });
                 console.log('Event added');
             });
