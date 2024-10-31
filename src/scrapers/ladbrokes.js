5c47f2c47297430258f966d5adb89c63bdbfe9e2
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-10-31 22:17:14 +1100

    added fs npm for data persistence (and testing)

--- src/scrapers/ladbrokes.js
+++ src/scrapers/ladbrokes.js
@@ -1,59 +1,60 @@
 import puppeteer from 'puppeteer';
+import { data } from '../data.js';
 
 export async function ladbrokesScraper(page, url, visitedLinks, queue) {
-	try {
-		await page.goto(url);
-		const sportCard = await page.$('#main #page .sport-event-card');
-		if (!sportCard) return;
-		
-		findUrls(page, visitedLinks, queue);
-		const events = await page.evaluate(() => {
-			function collectMarket(eventCard) {
-				// most likely these are the selectors for any sport event on the ladbrokes desktop site
-				const title = eventCard.querySelector('.sports-event-title__name-text');
-				const odds = eventCard.querySelectorAll('.price-button-odds-price span');
-				const names = eventCard.querySelectorAll('.price-button-name .displayTitle');
-				if (names.length == 2) {
-					return {
-						eventTitle: title.innerText.trim(),
-						team1Name: names[0].innerText.trim(),
-						team1Odds: odds[0].innerText.trim(),
-						team2Name: names[1].innerText.trim(),
-						team2Odds: odds[1].innerText.trim(),
-					};
-				} else if (names.length == 3) {
-					return {
-						eventTitle: title.innerText.trim(),
-						team1Name: names[0].innerText.trim(),
-						team1Odds: odds[0].innerText.trim(),
-						drawOdds: odds[1].innerText.trim(),
-						team2Name: names[2].innerText.trim(),
-						team2Odds: odds[2].innerText.trim(),
-					};
-				} else {
-					return 'names length is not 2 or 3';
-				}
-			}
-			const eventsArray = [];
-			const eventCards = document.querySelectorAll('#main #page .sport-event-card');
-			eventCards.forEach(eventCard => {
+  try {
+    await page.goto(url);
+    const sportCard = await page.$('#main #page .sport-event-card');
+    if (!sportCard) return;
+    
+    findUrls(page, visitedLinks, queue);
+    const events = await page.evaluate(() => {
+      function collectMarket(eventCard) {
+        // most likely these are the selectors for any sport event on the ladbrokes desktop site
+        const title = eventCard.querySelector('.sports-event-title__name-text');
+        const odds = eventCard.querySelectorAll('.price-button-odds-price span');
+        const names = eventCard.querySelectorAll('.price-button-name .displayTitle');
+        if (names.length == 2) {
+          return {
+            eventTitle: title.innerText.trim(),
+            team1Name: names[0].innerText.trim(),
+            team1Odds: odds[0].innerText.trim(),
+            team2Name: names[1].innerText.trim(),
+            team2Odds: odds[1].innerText.trim(),
+          };
+        } else if (names.length == 3) {
+          return {
+            eventTitle: title.innerText.trim(),
+            team1Name: names[0].innerText.trim(),
+            team1Odds: odds[0].innerText.trim(),
+            drawOdds: odds[1].innerText.trim(),
+            team2Name: names[2].innerText.trim(),
+            team2Odds: odds[2].innerText.trim(),
+          };
+        } else {
+          return 'names length is not 2 or 3';
+        }
+      }
+      const eventsArray = [];
+      const eventCards = document.querySelectorAll('#main #page .sport-event-card');
+      eventCards.forEach(eventCard => {
         const hasMarket = eventCard.querySelector('.sports-market-primary');
         if (!hasMarket) return;
         const event = collectMarket(eventCard);
         eventsArray.push(event);
-			});
-			return eventsArray;
-		});
-		return events;
-	} catch (error) {
-		console.log('Error fetching page: ', error);
-	}
+      });
+      return eventsArray;
+    });
+    return events;
+  } catch (error) {
+    console.log('Error fetching page: ', error);
+  }
 }
 
 async function findUrls(page, visitedLinks, queue) {
-	try {
-		const visitedLinksArray = Array.from(visitedLinks);
-		const pageUrls = await page.evaluate((visitedLinksArray) => {
+  try {
+    const visitedLinksArray = Array.from(visitedLinks);
+    const pageUrls = await page.evaluate((visitedLinksArray) => {
       function goodUrl(url) {
         const href = url.getAttribute('href');
     
@@ -79,17 +80,17 @@ async function findUrls(page, visitedLinks, queue) {
         urls.push(link.href);
       });
       return urls;
-		}, visitedLinksArray);
-		pageUrls.forEach(url => {
-				queue.enqueue(url);
-		});
-	} catch (error) {
-		console.log(error);
-	}
+    }, visitedLinksArray);
+    pageUrls.forEach(url => {
+        queue.enqueue(url);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export function getSportFromUrl(url) {
-  const sport = url.match(/\/sports\/([^/]+)/);
+  const sport = url.match(/\/sports\/([^/]+)/).toLowerCase();
   if (!sport) return null;
 
   return sport[1].replace(/-/g, "_");
