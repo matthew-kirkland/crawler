fae731fb8c55c612ce37a54ab465a50442aad731
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-11-18 23:05:54 +1100

    added test stubs and renamed file

---
+++ src/tests/database.test.js
@@ -0,0 +1,23 @@
+import { writeToData } from '../scrapers/ladbrokes.js';
+
+
+describe('Database testing', () => {
+  test('Empty database, first game added', async () => {
+    expect(1+1).toEqual(2);
+  });
+  test('Database with existing games, game added to empty sport array', async () => {
+    expect(1+1).toEqual(2);
+  });
+  test('Database with existing games, game added to populated sport array', async () => {
+    expect(1+1).toEqual(2);
+  });
+  test('Game exists in sport array, different website market', async () => {
+    expect(1+1).toEqual(2);
+  });
+  test('Game exists in sport array, website market already exists', async () => {
+    expect(1+1).toEqual(2);
+  });
+  test('Sport unrecognised', async () => {
+    expect(1+1).toEqual(2);
+  })
+});
