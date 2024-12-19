7043d702646bfb450c6a8053a1c04d8c9d01fe51
Matthew-Kirkland <matthewkirkland049@gmail.com>
2024-12-19 23:46:20 +1100

    calculator now spreads profit across both events and rounds to prevent suspicious bets

--- src/utils/Calculator.js
+++ src/utils/Calculator.js
@@ -3,23 +3,31 @@ export class Calculator {
     return ((1 / odds1) + (1 / odds2) < 1);
   }
 
-  static moneySplit(total, odds1, odds2) {
+  static splitMoney(total, odds1, odds2) {
+    const lowerBound = total / odds1;
+    const upperBound = total - total / odds2;
+    const average = (upperBound + lowerBound) / 2;
     let money1;
-    let money2;
-    if (odds2 >= odds1) {
-      money2 = total / odds2;
-      money1 = total - money2;
+    if (upperBound - lowerBound >= 10) {
+      const closestMultiple = Math.round(average / 10) * 10;
+      money1 = Math.min(Math.max(closestMultiple, Math.ceil(lowerBound / 10) * 10), Math.floor(upperBound / 10) * 10);
+    } else if (upperBound - lowerBound >= 5) {
+      const closestMultiple = Math.round(average / 5) * 5;
+      money1 = Math.min(Math.max(closestMultiple, Math.ceil(lowerBound / 5) * 5), Math.floor(upperBound / 5) * 5);
+    } else if (upperBound - lowerBound >= 1) {
+      const closestMultiple = Math.ceil(average);
+      money1 = Math.min(Math.max(closestMultiple, Math.ceil(lowerBound)), Math.floor(upperBound));
     } else {
-      money1 = total / odds1;
-      money2 = total - money1;
+      money1 = null;
     }
+    const money2 = total - money1;
     return {
       money1: money1,
       money2: money2,
     };
   }
 
-  static profit(odds1, odds2) {
+  static expectedProfit(odds1, odds2) {
     return (1 - (1 / odds1) - (1 / odds2));
   }
 }
